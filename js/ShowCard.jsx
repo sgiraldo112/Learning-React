3d46cdce71d92a0c7cd061bddfa2ec7d786bbd5b
Stephanie Giraldo <stephaniegiraldo@ufl.edu>
2018-01-10 14:30:50 -0500

    minor changes

--- js/ShowCard.jsx
+++ js/ShowCard.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { shape, string } from 'prop-types';
 
 const ShowCard = props => (
   <div className="show-card">
@@ -10,5 +11,12 @@ const ShowCard = props => (
     </div>
   </div>
 );
-
+ShowCard.propTyoes = {
+  show: shape({
+    poster: string.isRequired,
+    title: string.isRequired,
+    year: string.isRequired,
+    description: string.isRequired
+  }).isRequired
+};
 export default ShowCard;
